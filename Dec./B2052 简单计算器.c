95fec7304f8263bc5e3ce05d0bd73090ba5e13d6
cZanY714285 <firstczhuzany@163.com>
2024-12-10 11:06:17 +0800

    Add files via upload

---
+++ Dec./B2052 简单计算器.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main() {
+    int a,b;
+    char op;
+    scanf("%d %d %c",&a,&b,&op);
+
+    if (op == '+') {
+        printf("%d",a+b);
+    }else if (op == '-') {
+        printf("%d",a-b);
+    }else if (op == '*') {
+        printf("%d",a*b);
+    }else if (op == '/') {
+        if (b == 0) {
+            printf("Divided by zero!");
+        }else {
+            printf("%d",a/b);
+        }
+    }else {
+        printf("Invalid operator!");
+    }
+
+    return 0;
+}
